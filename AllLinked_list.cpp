b97085b91c3b84d9db204fd8a3fe489e9b9bc442
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-06-15 11:29:06 +0530

    Update AllLinked_list.cpp

--- AllLinked_list.cpp
+++ AllLinked_list.cpp
@@ -142,3 +142,59 @@ int length(Node *head){
     return count;
 }
 =================================================================
+
+7) REVERSE A LINKED LIST
+`````````````````````````
+time complexity: O(N)
+space complexity: O(N)
+
+Node* reverse(Node* head) {
+    if(head==NULL || head->next==NULL)
+    {
+        return head;
+    } 
+    Node *temp= reverse(head->next);
+    Node *tail=head->next;
+    tail->next=head;
+    head->next=NULL;
+    return temp;
+}
+
+---------------OR---------------------------------------------
+/*
+Iterative Method 
+
+Initialize three pointers prev as NULL, curr as head and next as NULL.
+Iterate through the linked list. In loop, do following. 
+// Before changing next of current, 
+// store next node 
+next = curr->next
+// Now change next of current 
+// This is where actual reversing happens 
+curr->next = prev 
+// Move prev and curr one step forward 
+prev = curr 
+curr = next
+*/
+ void reverse()
+    {
+        // Initialize current, previous and
+        // next pointers
+        Node* current = head;
+        Node *prev = NULL, *next = NULL;
+ 
+        while (current != NULL) {
+            // Store next
+            next = current->next;
+ 
+            // Reverse current node's pointer
+            current->next = prev;
+ 
+            // Move pointers one position ahead.
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+=================================================================	
+
