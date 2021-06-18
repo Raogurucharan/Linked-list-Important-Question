d46a11c858ad3111350c73479926ec6c503b51e9
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-06-18 11:51:58 +0530

    Update AllLinked_list.cpp

--- AllLinked_list.cpp
+++ AllLinked_list.cpp
@@ -304,3 +304,92 @@ return head;
 }
 
 ====================================================================
+
+12) CHECK IF THE LINKED LIST IS PALINDROME OR NOT
+``````````````````````````````````````````````````
+bool isPalin(Node* head){
+
+        Node* temp= head; 
+        stack <int> s;
+        while(temp != NULL){
+                s.push(temp->data);
+                 temp = temp->ptr;
+        }
+        while(head != NULL ){  
+             int i=s.top();
+             s.pop();   
+            if(head -> data != i){
+                return false;
+            }
+           head=head->ptr;
+        }
+return true;
+}
+
+````````````````````````OR`````````````````````````````````
+/*
+Given a singly linked list, determine if its a palindrome. Return 1 or 0 denoting if its a palindrome or not, respectively.
+
+Notes: 
+- Expected solution is linear in time and constant in space.
+
+For example,
+
+List 1-->2-->1 is a palindrome.
+List 1-->2-->3 is not a palindrome.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+ int findLength(ListNode *temp){
+     int l=0;
+     while(temp!=NULL){
+         l++;
+         temp=temp->next;
+     }
+     return l;
+ }
+ 
+int Solution::lPalin(ListNode* A) {
+    int len=findLength(A);
+    ListNode *mid=A;
+    int half;
+    if(len&1){
+        half=len/2+1;
+    }
+    else half=len/2;
+    ListNode *prev=NULL;
+    for(int i=0;i<half;i++){
+     prev=mid;
+     mid=mid->next;
+    }
+    prev->next=NULL;
+    
+    prev=NULL;
+    ListNode *curr=mid;
+    ListNode *next=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    
+    ListNode* t1=A;
+    ListNode*t2=prev;
+    while(t1!=NULL&&t2!=NULL){
+        if(t1->val!=t2->val)
+         return 0;
+        t1=t1->next;
+        t2=t2->next;
+    }
+    return 1;
+}
+========================================================
